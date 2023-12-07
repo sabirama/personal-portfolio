bb585cc644d6292c8a2205f0e814d519bc6a3e24
sabirama <markcondes21@gmail.com>
2023-12-07 23:00:57 +0800

    footer fix

--- src/components/home/subcomponents/services/subcomponents/BackEnd.jsx
+++ src/components/home/subcomponents/services/subcomponents/BackEnd.jsx
@@ -7,7 +7,7 @@ const BackEnd = (prop) => {
         <h4>Back-End Development</h4>
         <p>Create a server-side software, which focuses on everything you can`t see on a website.</p>
       </div>
-      <img src="/icons/back-end.svg" alt="bac-end" className="card-img" />
+      <img src="./icons/back-end.svg" alt="back-end" className="card-img" />
       <div className="overlay">
         <h4 className="overlay-text">
           <h3>More Details...</h3>
